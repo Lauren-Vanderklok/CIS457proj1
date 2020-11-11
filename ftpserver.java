dff4b246c27140c5cd63748e77e84776655cae92
Scott Weaver <weaversc@dc05.cis.gvsu.edu>
2020-11-11 16:57:21 -0500

    followed the samlpe code

--- ftpserver.java
+++ ftpserver.java
@@ -5,141 +5,106 @@ import java.text.*;
 import java.lang.*;
 import javax.swing.*;
 
-public class ftpserver extends Thread {
+    public class ftpserver extends Thread{ 
+        
+        /** controlSocket that reads and sends command outputs to the client */
+        private Socket connectionSocket;
 
-    /** controlSocket that reads and sends command outputs to the client */
-    Socket controlSocket;
-
-    int port = 8909;
-    int count = 1;
-
-
-    /** Stream that holds whatever output goes to the client */
-    DataOutputStream outputClient;
-
-    /** Stream that holds what input was taken from the client */
-    BufferedReader inputClient;
-
-    /** List of Strings
-     *      holds the commands given by the client because more than
-     *      one input/command is supported */
-    List<String> clientCommands;
-
-    /** Holds whatever command that came from the client */
-    String command;
-
-    /** Checks to see if the connection is still being held */
-    boolean serverRun = true;
-
-    Socket connectionSocket;
-
-    public ftpserver(Socket connectionSocket)  {
-
-        this.connectionSocket = connectionSocket;
+        int port = 8909;
+        int count=1;
+   
+        public ftpserver(Socket connectionSocket)  {
+	        this.connectionSocket = connectionSocket;
     }
 
 
+      public void run() 
+        {
+                if(count==1)
+                    System.out.println("User connected" + connectionSocket.getInetAddress());
+                count++;
 
-
-    public void run() {
-
-        if(count==1)
-            System.out.println("User connected" + connectionSocket.getInetAddress());
-        count++;
-
-        try {
-            processRequest();
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-
-
-    private void processRequest() throws Exception{
-
-        String fromClient;
-        //String clientCommand;
-        byte[] data;
-        String frstln;
-
-        while (serverRun) {
-            outputClient = new DataOutputStream(connectionSocket.getOutputStream());
-            inputClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-
-            fromClient = inputClient.readLine();
-            StringTokenizer tokens = new StringTokenizer(fromClient);
-
-            frstln = tokens.nextToken();
-            port = Integer.parseInt(frstln);
-            command = tokens.nextToken();
-
-            //list command //Lauren: this is scott's list command, which i have not edited, the samplecode has its own so we may want to change it
-            if (command.equals("list:")) {
-                //list command should only have 1 input, "list"
-                //thus get the first String in the list
-                Socket dataSocket = new Socket (connectionSocket.getInetAddress(), port);
-                        //makeServerSocket(Integer.parseInt(clientCommands.get(0)));
-                DataOutputStream dataOutToClient = new DataOutputStream(dataSocket.getOutputStream());
-
-                String files = listFiles();
-
-                //send the list of files to the client
-                dataOutToClient.writeUTF(files);
-
-                //clean up the stream before closing.
-                dataOutToClient.flush();
-                dataOutToClient.close();
-                System.out.println("eof");
-                dataSocket.close();
-                System.out.println("Data Socket closed.\n");
-            }
-            else if (command.equals("get: ")){}
-            else if (command.equals("stor: ")){}
-
-
-        }
-
-    }
-
-
-
-
-
-    /******************************************************************
-	 * Creates a server socket for the server and client to send data
-	 * over.
-	 * 
-	 * @param port 
-         *      The port the server will run on.
-	 * @return Socket 
-         *      The dataSocket object that was created.
-	 *****************************************************************/
-    private Socket makeServerSocket(int port) {
-        System.out.println("Making data socket...");
-            try {
-                return new Socket(controlSocket.getInetAddress(), port);
-            } catch (IOException e) {
-                System.out.println("Error " + e.toString());
-            }
-            return null;
-    }
-
-    /******************************************************************
-	 * Returns a list of all the files in the current directory.
-	 * 
-	 * @return list 
-         *      A string of all the files.
-	 *****************************************************************/
-	private static String listFiles() {
-		File curDir = new File(".");
-		File[] files = curDir.listFiles();
-		String list = "";
+	try {
+		processRequest();
 		
-		for (int i = 0; i < files.length; i++)
-			if (files[i].isFile())
-				list += files[i].getName() + "\n";
-		return list;
+	} catch (Exception e) {
+		System.out.println(e);
 	}
+	 
+	}
+	
+	
+	private void processRequest() throws Exception
+	{
+            String fromClient;
+            String clientCommand;
+            byte[] data;
+            String frstln;
+                    
+            while(true)
+            {
+                if(count==1)
+                    System.out.println("User connected" + connectionSocket.getInetAddress());
+                count++;
+         
+                DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                fromClient = inFromClient.readLine();
+            
+      		    //System.out.println(fromClient);
+                StringTokenizer tokens = new StringTokenizer(fromClient);
+            
+                frstln = tokens.nextToken();
+                port = Integer.parseInt(frstln);
+                clientCommand = tokens.nextToken();
+                //System.out.println(clientCommand);
+
+
+                  if(clientCommand.equals("list:")) { 
+                    String curDir = System.getProperty("user.dir");
+       
+                    Socket dataSocket = new Socket(connectionSocket.getInetAddress(), port);
+                    DataOutputStream  dataOutToClient = 
+                    new DataOutputStream(dataSocket.getOutputStream());
+                    File dir = new File(curDir);
+    
+                    String[] children = dir.list();
+                    if(children == null) 
+                      {
+                          // Either dir does not exist or is not a directory
+                      } 
+                      else 
+                      {
+                          for (int i=0; i<children.length; i++)
+                          {
+                            // Get filename of file or directory
+                            String filename = children[i];
+
+                            if(filename.endsWith(".txt"))
+                                dataOutToClient.writeUTF(children[i]);
+                                //System.out.println(filename);
+                            if(i-1==children.length-2)
+                            {
+                                dataOutToClient.writeUTF("eof");
+                                // System.out.println("eof");
+                            }//if(i-1)
+
+     
+                          }//for
+
+                           dataSocket.close();
+		            //System.out.println("Data Socket closed");
+                    }//else
+        
+
+                }//if list:
+
+
+                if(clientCommand.equals("get:"))
+                {
+             
+
+            }
+            //main
+        }
 }
