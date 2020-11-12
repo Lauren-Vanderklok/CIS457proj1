f23e79dc03141a1f6be10f1c88fd1469040c5ec0
Scott Weaver <weaversc@dc09.cis.gvsu.edu>
2020-11-12 01:41:55 -0500

    java styling

--- ftpmulti.java
+++ ftpmulti.java
@@ -1,6 +1,14 @@
 import java.net.*;
 import java.io.*;
 
+/**********************************************************************
+* FTPmulti class that creates a socket and or thread
+* on the server side so that communication can occur between the 
+* client and the server.
+*
+* @author Christian Thompson, James Weitzmanm, Josh Hubbard, 
+*         Lauren Vanderklok, & Scott Weaver 
+**********************************************************************/
 public class ftpmulti {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
@@ -8,23 +16,20 @@ public class ftpmulti {
 	    ftpserver thread;
         int port = 8907;
         try {
+            //create a socket with the given port parameter
             serverSocket = new ServerSocket(port);
         } catch (IOException e) {
             System.err.println("Could not listen on port: " + port);
             System.exit(-1);
         }
 
-	while (listening)
-	{
-	    
-	    thread = new ftpserver(serverSocket.accept());
-	    Thread t = new Thread(thread);
-	    t.start();
-	   
-	   
-	}
-
-       
-    }
+        //while connection has been made and is still open
+	    while (listening)
+	    {
+	        //create new thread on the socket that was created
+	        thread = new ftpserver(serverSocket.accept());
+	        Thread t = new Thread(thread);
+	        t.start();}
+     }
 }
 
