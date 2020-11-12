e3c24557d56c49acc6820bb7696a0181fb513a03
Scott Weaver <weaversc@dc07.cis.gvsu.edu>
2020-11-11 21:11:30 -0500

    put the stor command in the while loop that accounts for EOFException

--- ftpserver.java
+++ ftpserver.java
@@ -138,21 +138,19 @@ import javax.swing.*;
                     byte[] dataIn = new byte[clientInput.readByte()];
 
                     //added due to java.io.EOFException
-                    while(clientInput.available() == 0) {
-                        Thread.sleep(10);
-                    }
+                    while(clientInput.available() != 0) {
+                         //get the working directory
+                        String filePath = System.getProperty("user.dir") + "/";
+                        //get the file name from the client
+                        String fileName = clientInput.readUTF();
+                        filePath += fileName;
 
-                    //get the working directory
-                    String filePath = System.getProperty("user.dir") + "/";
-                    //get the file name from the client
-                    String fileName = clientInput.readUTF();
-                    filePath += fileName;
-
-                    System.out.println("Storing " + fileName + " in the current directory")
+                        System.out.println("Storing " + fileName + " in the current directory")
                     
-                    //write bytes to file
-                    try (FileOutputStream fos = new FileOutputStream(filePath)) {
-                        fos.write(dataIn);
+                        //write bytes to file
+                        try (FileOutputStream fos = new FileOutputStream(filePath)) {
+                            fos.write(dataIn);
+                        }
                     }
 
                     //stor has been performed, terminate the connection
